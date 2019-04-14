b4c2b86d2612c17bbb915f060340543c2c92c9fd
CitruXonve <silveritdean@gmail.com>
2019-04-14 07:55:18 -0700

    Clean up for submitting

--- server/server.js
+++ server/server.js
@@ -9,15 +9,10 @@ exports.Server = class {
         this.port = port;
 
         http.createServer((req, res) => {
-            // console.log(req)
 
-            // res.setHeader("Access-Control-Allow-Origin", "*");
             res.writeHead(200, { 'Content-Type': 'application/json', "Access-Control-Allow-Origin": "*"});
-            // res.setHeader("Access-Control-Allow-Origin", "*");
-
             
             if (req.method && req.method==='GET'){
-                // res.end(JSON.stringify({"result":"Some data", "method":req.method, "URL": req.url}))
 
                 const events = new datahandler.DataHandler("./location.json").retrieve();
 
@@ -27,18 +22,9 @@ exports.Server = class {
                 else if (url.parse(req.url).pathname && url.parse(req.url).pathname==='/query_event'){
                     var params = new URLSearchParams(url.parse(req.url).query);
                     
-                    console.log(params)
-                    // console.log(params.get('delta_time'),params.get('latitude'))
+                    // console.log(params)
                     var filtered_events = events;
                     if (params.get('delta_time') && eval(params.get('delta_time'))>=0.0){
-                        // var delta_time = params.get('delta_time');
-                        // filtered_events.forEach(event => {
-                        //     var current_time = new Date().getTime();
-                        //     var delta_time = eval(params.get('delta_time'))
-                        //     console.log(eval(event.start_time) , current_time+delta_time , 
-                        //      eval(event.end_time), event.start_time <= current_time+delta_time , 
-                        //      current_time + delta_time <= eval(event.end_time));
-                        // });
                         filtered_events = filtered_events.filter((event) => {
                             var current_time = new Date().getTime();
                             var delta_time = eval(params.get('delta_time'))
@@ -76,7 +62,6 @@ exports.Server = class {
                     body += data
                 })
                 req.on('end', () => {
-                    // res.writeHead(200, {'Content-Type': 'text/html'})
                     res.end(JSON.stringify({"result":"Some data", "method":req.method, "content": body}))
                 })
                 
